74a4423fcfcedcaea1d2c3dfd4b17a5164454ebd
Sluijsens <bdslop@gmail.com>
2014-12-23 00:04:46 +0100

    started adding custom keyboard input map

---
+++ BroSloRemoteMouse/app/src/main/java/nl/broslo/brosloremotemouse/utility/KeyCodeAndroidToJava.java
@@ -0,0 +1,89 @@
+package nl.broslo.brosloremotemouse.utility;
+
+import android.view.KeyEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Bryan on 22-12-2014.
+ */
+public static class KeyCodeAndroidToJava {
+
+    public static final Map<Integer, Integer> keyCodeMap = new HashMap<Integer, Integer>();
+
+    static {
+
+        /**
+         * Add numeric characters
+         */
+        keyCodeMap.put(KeyEvent.KEYCODE_0, );
+        keyCodeMap.put(KeyEvent.KEYCODE_1, );
+        keyCodeMap.put(KeyEvent.KEYCODE_2, );
+        keyCodeMap.put(KeyEvent.KEYCODE_3, );
+        keyCodeMap.put(KeyEvent.KEYCODE_4, );
+        keyCodeMap.put(KeyEvent.KEYCODE_5, );
+        keyCodeMap.put(KeyEvent.KEYCODE_6, );
+        keyCodeMap.put(KeyEvent.KEYCODE_7, );
+        keyCodeMap.put(KeyEvent.KEYCODE_8, );
+        keyCodeMap.put(KeyEvent.KEYCODE_9, );
+
+        /**
+         * Add alphabetic characters
+         */
+        keyCodeMap.put(KeyEvent.KEYCODE_A, );
+        keyCodeMap.put(KeyEvent.KEYCODE_B, );
+        keyCodeMap.put(KeyEvent.KEYCODE_C, );
+        keyCodeMap.put(KeyEvent.KEYCODE_D, );
+        keyCodeMap.put(KeyEvent.KEYCODE_E, );
+        keyCodeMap.put(KeyEvent.KEYCODE_F, );
+        keyCodeMap.put(KeyEvent.KEYCODE_G, );
+        keyCodeMap.put(KeyEvent.KEYCODE_H, );
+        keyCodeMap.put(KeyEvent.KEYCODE_I, );
+        keyCodeMap.put(KeyEvent.KEYCODE_J, );
+        keyCodeMap.put(KeyEvent.KEYCODE_K, );
+        keyCodeMap.put(KeyEvent.KEYCODE_L, );
+        keyCodeMap.put(KeyEvent.KEYCODE_M, );
+        keyCodeMap.put(KeyEvent.KEYCODE_N, );
+        keyCodeMap.put(KeyEvent.KEYCODE_O, );
+        keyCodeMap.put(KeyEvent.KEYCODE_P, );
+        keyCodeMap.put(KeyEvent.KEYCODE_Q, );
+        keyCodeMap.put(KeyEvent.KEYCODE_R, );
+        keyCodeMap.put(KeyEvent.KEYCODE_S, );
+        keyCodeMap.put(KeyEvent.KEYCODE_T, );
+        keyCodeMap.put(KeyEvent.KEYCODE_U, );
+        keyCodeMap.put(KeyEvent.KEYCODE_V, );
+        keyCodeMap.put(KeyEvent.KEYCODE_W, );
+        keyCodeMap.put(KeyEvent.KEYCODE_X, );
+        keyCodeMap.put(KeyEvent.KEYCODE_Y, );
+        keyCodeMap.put(KeyEvent.KEYCODE_Z, );
+
+        /**
+         * Add special characters
+         */
+        keyCodeMap.put(KeyEvent.KEYCODE_SPACE , );
+        keyCodeMap.put(KeyEvent.KEYCODE_TAB, );
+        keyCodeMap.put(KeyEvent.KEYCODE_APOSTROPHE, );
+        keyCodeMap.put(KeyEvent.KEYCODE_AT, );
+        keyCodeMap.put(KeyEvent.KEYCODE_NUMPAD_ADD, );
+        keyCodeMap.put(KeyEvent.KEYCODE_BACKSLASH, );
+        keyCodeMap.put(KeyEvent.KEYCODE_LEFT_BRACKET, );
+        keyCodeMap.put(KeyEvent.KEYCODE_RIGHT_BRACKET, );
+        keyCodeMap.put(KeyEvent.KEYCODE_COMMA, );
+        keyCodeMap.put(KeyEvent.KEYCODE_NUMPAD_COMMA, );
+        keyCodeMap.put(KeyEvent.KEYCODE_NUMPAD_DIVIDE, );
+        keyCodeMap.put(KeyEvent.KEYCODE_NUMPAD_DOT, );
+        keyCodeMap.put(KeyEvent.KEYCODE_EQUALS, );
+        keyCodeMap.put(KeyEvent.KEYCODE_NUMPAD_EQUALS, );
+        keyCodeMap.put(KeyEvent.KEYCODE_GRAVE, );
+        keyCodeMap.put(KeyEvent.KEYCODE_NUMPAD_MULTIPLY, );
+        keyCodeMap.put(KeyEvent.KEYCODE_NUMPAD_SUBTRACT, );
+
+        /**
+         * Add "special" keyboard keys
+         */
+        keyCodeMap.put(KeyEvent.KEYCODE_, );
+        keyCodeMap.put(KeyEvent.KEYCODE_, );
+    }
+
+}
